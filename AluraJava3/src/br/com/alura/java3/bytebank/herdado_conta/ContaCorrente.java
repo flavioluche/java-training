715782bcddbf1b544b868eb9c1b9c0f04e644269
Flavio Luche <flavioluche@gmail.com>
2019-06-15 16:31:17 -0300

    deletes .metadata and .recommenders. Creates a new package with 2 classes extending class conta and creating its exclusive constructor

---
+++ AluraJava3/src/br/com/alura/java3/bytebank/herdado_conta/ContaCorrente.java
@@ -0,0 +1,7 @@
+package br.com.alura.java3.bytebank.herdado_conta;
+
+public class ContaCorrente extends Conta {
+	public ContaCorrente(int agencia, int conta, int dac) {
+		super(agencia, conta, dac);
+	}
+}
