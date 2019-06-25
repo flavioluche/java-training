2f50a9f2ec3c45e4c339c8d215a0ae98797de55f
Flavio Luche <flavioluche@gmail.com>
2019-06-25 00:30:53 -0300

    update class to test

--- Java4/ExceptionsJava/src/exceptionsJava/TestaConexao.java
+++ Java4/ExceptionsJava/src/exceptionsJava/TestaConexao.java
@@ -4,13 +4,12 @@ public class TestaConexao {
 
 	public static void main(String[] args) {
 			
-		try (Conexao con =new Conexao()){
+		try {
+			Conexao con = new Conexao();
 			con.leDados();
 			con.fecha();
 		} catch (IllegalStateException ex) {
 			System.out.println("Falha na conexão");
-		} finally {
-			con.fecha();
 		}
 			
 			
